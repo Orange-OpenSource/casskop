8ad407f42ef987829ee986ad43b0dc01b59e2234
sebastien allamand <sebastien.allamand@orange.com>
2019-06-20 17:17:35 +0200

    Add unit-test function using fake client to init a fake cluster
    Signed-off-by: sebastien allamand <sebastien.allamand@orange.com>

--- pkg/controller/cassandracluster/cassandra_status_test.go
+++ pkg/controller/cassandracluster/cassandra_status_test.go
@@ -15,14 +15,27 @@
 package cassandracluster
 
 import (
+	"context"
 	"fmt"
+	"k8s.io/api/core/v1"
+	//"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	"reflect"
+	appsv1 "k8s.io/api/apps/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	//"k8s.io/client-go/kubernetes/scheme"
+	//"sigs.k8s.io/controller-runtime/pkg/client/fake"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"strconv"
 	"testing"
 
+	api "github.com/Orange-OpenSource/cassandra-k8s-operator/pkg/apis/db/v1alpha1"
 	"github.com/ghodss/yaml"
 	"github.com/stretchr/testify/assert"
-	api "github.com/Orange-OpenSource/cassandra-k8s-operator/pkg/apis/db/v1alpha1"
 )
+
+var name string = "cassandra-demo"
+var namespace string = "ns"
 
 var cc2Dcs = `
 apiVersion: "db.orange.com/v1alpha1"
@@ -51,7 +64,7 @@ spec:
       memory: 2Gi
   topology:
     dc:
-      - name: online
+      - name: dc1
         labels:
           location.dfy.orange.com/site : mts
         rack:
@@ -61,7 +74,7 @@ spec:
           - name: rack2
             labels: 
               location.dfy.orange.com/street : street2
-      - name: stats
+      - name: dc2
         nodesPerRacks: 2
         labels: 
           location.dfy.orange.com/site : mts
@@ -81,21 +94,153 @@ func TestUpdateStatusIfSeedListHasChanged(t *testing.T) {
 	}
 	cc.InitCassandraRackList()
 
-	assert.Equal(api.ClusterPhaseInitial, cc.Status.CassandraRackStatus["online-rack1"].CassandraLastAction.Name)
-	assert.Equal(api.ClusterPhaseInitial, cc.Status.CassandraRackStatus["online-rack2"].CassandraLastAction.Name)
-	assert.Equal(api.ClusterPhaseInitial, cc.Status.CassandraRackStatus["stats-rack1"].CassandraLastAction.Name)
+	assert.Equal(api.ClusterPhaseInitial, cc.Status.CassandraRackStatus["dc1-rack1"].CassandraLastAction.Name)
+	assert.Equal(api.ClusterPhaseInitial, cc.Status.CassandraRackStatus["dc1-rack2"].CassandraLastAction.Name)
+	assert.Equal(api.ClusterPhaseInitial, cc.Status.CassandraRackStatus["dc2-rack1"].CassandraLastAction.Name)
 	assert.Equal(3, len(cc.Status.CassandraRackStatus))
 
 	cc.Status.SeedList = cc.InitSeedList()
 
-	var a = []string{"cassandra-demo-online-rack1-0.cassandra-demo-online.ns",
-		"cassandra-demo-online-rack1-1.cassandra-demo-online.ns",
-		"cassandra-demo-online-rack2-0.cassandra-demo-online.ns",
-		"cassandra-demo-stats-rack1-0.cassandra-demo-stats.ns",
-		"cassandra-demo-stats-rack1-1.cassandra-demo-stats.ns"}
+	var a = []string{"cassandra-demo-dc1-rack1-0.cassandra-demo-dc1.ns",
+		"cassandra-demo-dc1-rack1-1.cassandra-demo-dc1.ns",
+		"cassandra-demo-dc1-rack2-0.cassandra-demo-dc1.ns",
+		"cassandra-demo-dc2-rack1-0.cassandra-demo-dc2.ns",
+		"cassandra-demo-dc2-rack1-1.cassandra-demo-dc2.ns"}
 
 	assert.Equal(5, len(cc.Status.SeedList))
 
 	assert.Equal(true, reflect.DeepEqual(a, cc.Status.SeedList))
+
+}
+
+//helperCreateCassandraCluster fake create a cluster from the yaml specified
+func helperCreateCassandraCluster(t *testing.T, cassandraClusterFileName string) (*ReconcileCassandraCluster,
+	*reconcile.Request) {
+	assert := assert.New(t)
+	rcc, cc := helperInitCluster(t, cassandraClusterFileName)
+
+
+	req := reconcile.Request{
+		NamespacedName: types.NamespacedName{
+			Name:      cc.Name,
+			Namespace: cc.Namespace,
+		},
+	}
+
+	//The first Reconcile Just make Init
+	res, err := rcc.Reconcile(req)
+	if err != nil {
+		t.Fatalf("reconcile: (%v)", err)
+	}
+	err = rcc.client.Get(context.TODO(), req.NamespacedName, cc)
+	if err != nil {
+		t.Fatalf("can't get cassandracluster: (%v)", err)
+	}
+	//Check that we have set the Finalizers
+	f := cc.GetFinalizers()
+	assert.Equal(f[0], "kubernetes.io/pvc-to-delete", "set finalizer for PVC")
+	// Check the result of reconciliation to make sure it has the desired state.
+	if !res.Requeue {
+		t.Error("reconcile did not requeue request as expected")
+	}
+
+	//Second Reconcile create objects
+	res, err = rcc.Reconcile(req)
+	if err != nil {
+		t.Fatalf("reconcile: (%v)", err)
+	}
+
+	dcRackNames := cc.GetDCRackNames()
+	for _, dcRackName := range dcRackNames{
+		//Update Statefulset fake status
+		sts := &appsv1.StatefulSet{}
+		err = rcc.client.Get(context.TODO(), types.NamespacedName{Name: cc.Name+"-"+dcRackName,
+			Namespace: cc.Namespace},
+		sts)
+		if err != nil {
+			t.Fatalf("get statefulset: (%v)", err)
+		}
+		// Check if the quantity of Replicas for this deployment is equals the specification
+		dsize := *sts.Spec.Replicas
+		if dsize != 1 {
+			t.Errorf("dep size (%d) is not the expected size (%d)", dsize, cc.Spec.NodesPerRacks)
+		}
+		//Now simulate sts to be ready for CassKop
+		sts.Status.Replicas = *sts.Spec.Replicas
+		sts.Status.ReadyReplicas = *sts.Spec.Replicas
+		rcc.UpdateStatefulSet(sts)
+
+		//Create Statefulsets associated fake Pods
+		pod := &v1.Pod{
+			TypeMeta: metav1.TypeMeta{
+				Kind:       "Pod",
+				APIVersion: "v1",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "template",
+				Namespace: namespace,
+			},
+			Status: v1.PodStatus{
+				Phase: v1.PodRunning,
+				ContainerStatuses: []v1.ContainerStatus{
+					v1.ContainerStatus{
+						Name: "cassandra",
+						Ready: true,
+					},
+				},
+			},
+		}
+
+		for i := 0; i < int(sts.Status.Replicas); i++{
+			pod.Name = sts.Name+strconv.Itoa(i)
+			err = rcc.CreatePod(pod)
+			if err != nil {
+				t.Fatalf("can't create pod: (%v)", err)
+			}
+		}
+
+		//We recall Reconcile to update Next rack
+		res, err = rcc.Reconcile(req)
+		if err != nil {
+			t.Fatalf("reconcile: (%v)", err)
+		}
+	}
+
+	//Check creation Statuses
+	err = rcc.client.Get(context.TODO(), req.NamespacedName, cc)
+	if err != nil {
+		t.Fatalf("can't get cassandracluster: (%v)", err)
+	}
+	assert.Equal(cc.Status.Phase, api.ClusterPhaseRunning)
+
+	for _, dcRackName := range dcRackNames{
+		assert.Equal(cc.Status.CassandraRackStatus[dcRackName].Phase, api.ClusterPhaseRunning)
+		assert.Equal(cc.Status.CassandraRackStatus[dcRackName].CassandraLastAction.Name, api.ClusterPhaseInitial)
+		assert.Equal(cc.Status.CassandraRackStatus[dcRackName].CassandraLastAction.Status, api.StatusDone)
+	}
+			assert.Equal(cc.Status.LastClusterAction, api.ClusterPhaseInitial)
+		assert.Equal(cc.Status.LastClusterActionStatus, api.StatusDone)
+
+	return rcc, &req
+}
+
+func TestReconcileCassandraCluster(t *testing.T) {
+	//assert := assert.New(t)
+
+	// Mock request to simulate Reconcile() being called on an event for a
+	// watched resource .
+
+	rcc, req := helperCreateCassandraCluster(t, "cassandracluster-2DC.yaml")
+
+	//WARNING: ListPod with fieldselector is not working on client-side
+	//So CassKop will try to execute podActions in pods without succeed (they are fake pod)
+	//https://github.com/kubernetes/client-go/issues/326
+	res, err := rcc.Reconcile(*req)
+	if err != nil {
+		t.Fatalf("reconcile: (%v)", err)
+	}
+	if !res.Requeue && res.RequeueAfter == 0 {
+		t.Error("reconcile did not requeue request as expected")
+	}
 
 }
