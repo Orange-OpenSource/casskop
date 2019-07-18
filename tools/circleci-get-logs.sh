b8b28e52c2a0f895f1886e5f5fa73f360968b843
sebastien allamand <sebastien.allamand@orange.com>
2019-07-18 10:26:36 +0200

    increase time for scaledown check
    Signed-off-by: sebastien allamand <sebastien.allamand@orange.com>

--- tools/circleci-get-logs.sh
+++ tools/circleci-get-logs.sh
@@ -4,12 +4,11 @@ while true; do
     NAME=$(kubectl -n cassandra-e2e get pods -l app=cassandra-k8s-operator -o jsonpath='{range .items[*]}{.metadata.name}');
     READY=$(kubectl -n cassandra-e2e get pods -l app=cassandra-k8s-operator -o jsonpath='{range .items[*]}{.status.containerStatuses[0].ready}');
 
-    echo "wait for logs: $NAME=$READY";
-    sleep 3;
-
     if [[ "$NAME" != "" ]] && [[ "$READY" == "true" ]]; then
         break;
     fi
+    echo "wait for logs: $NAME=$READY";
+    sleep 10;
 done ;
 
 echo "Get Operator logs: $NAME";
