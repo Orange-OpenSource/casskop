408f1385371a2fb8f6e00d147835268d6170ca3a
Sébastien Allamand <sebastien.allamand@orange.com>
2019-10-15 17:15:50 +0200

    fix issue where several was updated in all racks at the same time (#127)
    * fix issue where several was updated in all racks at the same time
    * remove circleci docker_layer_caching: true
    * increase timeout to 5mn to wait for minikube

--- pkg/controller/cassandracluster/deploy_cassandra.go
+++ pkg/controller/cassandracluster/deploy_cassandra.go
@@ -86,17 +86,17 @@ func (rcc *ReconcileCassandraCluster) ensureCassandraPodDisruptionBudget(cc *api
 // take dcRackName to accordingly named the statefulset
 // take dc and rack index of dc and rack in conf to retrieve according  nodeselectors labels
 func (rcc *ReconcileCassandraCluster) ensureCassandraStatefulSet(cc *api.CassandraCluster,
-	status *api.CassandraClusterStatus, dcName string, dcRackName string, dc int, rack int) error {
+	status *api.CassandraClusterStatus, dcName string, dcRackName string, dc int, rack int) (bool, error) {
 
 	labels, nodeSelector := k8s.GetDCRackLabelsAndNodeSelectorForStatefulSet(cc, dc, rack)
 
 	ss := generateCassandraStatefulSet(cc, status, dcName, dcRackName, labels, nodeSelector, nil)
 	k8s.AddOwnerRefToObject(ss, k8s.AsOwner(cc))
 
-	err := rcc.CreateOrUpdateStatefulSet(ss, status, dcRackName)
+	breakResyncloop, err := rcc.CreateOrUpdateStatefulSet(ss, status, dcRackName)
 	if err != nil && !apierrors.IsAlreadyExists(err) {
-		return fmt.Errorf("failed to create cassandra statefulset: %v", err)
+		return breakResyncloop, fmt.Errorf("failed to create cassandra statefulset: %v", err)
 	}
 
-	return nil
+	return breakResyncloop, nil
 }
