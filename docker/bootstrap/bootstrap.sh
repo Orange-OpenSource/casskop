96d32be41b277f89b7d51740c45803d078c2c94d
Sébastien Allamand <sebastien.allamand@orange.com>
2019-10-15 23:00:30 +0200

    add annotation for pod & services (#124)
    * add annotation for pod & services
    * add post_run.sh script execution from ConfigMap in bootstrap image
    * add tests
    * Remove docker caching
    * delete deepcopy

--- docker/bootstrap/bootstrap.sh
+++ docker/bootstrap/bootstrap.sh
@@ -6,23 +6,29 @@ cp -rLv /${BOOTSTRAP_CONF}/* /etc/cassandra/
 
 # If User has submited a configMap, we uses them to deplace default ones
 # (overwriting the above)
-if [[ -d /${CONFIGMAP} && ! -z `ls -A /${CONFIGMAP}` ]] ; then
-    echo "We have a ConfigMap, we surcharge default configuration files"
-    cp -rLv /${CONFIGMAP}/* /etc/cassandra/
+if [[ -d ${CONFIGMAP} && ! -z `ls -A ${CONFIGMAP}` ]] ; then
+    echo " == We have a ConfigMap, we surcharge default configuration files"
+    cp -rLv ${CONFIGMAP}/* /etc/cassandra/
 fi
 
 # Copies any extra libraries from this bootstrapper image to the extra-lib empty-dir
 if [[ -d /${BOOTSTRAP_LIBS} && ! -z `ls -A /${BOOTSTRAP_LIBS}` ]] ; then
-    echo "We have a Additional libs, we surcharge default configuration files"
+    echo " == We have a Additional libs, we surcharge default configuration files"
     cp -v ${BOOTSTRAP_LIBS}/* $CASSANDRA_LIBS/
    fi
 
 if [ -f ${CONFIGMAP}/pre_run.sh ]; then
-    echo "We found pre_run.sh script, we execute it"
+    echo " == We found pre_run.sh script, we execute it"
     ${CONFIGMAP}/pre_run.sh
 fi
 
 # Bootstrap Cassandra configuration
+echo " == We execute bootstrap srcipt run.sh"
 /${BOOTSTRAP_CONF}/run.sh
 
-echo 'bootstrap ended :-)'
+if [ -f ${CONFIGMAP}/post_run.sh ]; then
+    echo " == We found post_run.sh script, we execute it"
+    ${CONFIGMAP}/post_run.sh
+fi
+
+echo '== bootstrap ended :-)'
