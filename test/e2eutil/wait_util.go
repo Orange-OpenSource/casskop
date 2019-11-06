1d62555ce1f94aaaf04b2bd23133f3c08889a406
sebastien allamand <sebastien.allamand@orange.com>
2019-11-06 16:43:41 +0100

    force command in casskop to avoid execution of entrypoints

--- test/e2eutil/wait_util.go
+++ test/e2eutil/wait_util.go
@@ -32,7 +32,7 @@ import (
 
 var (
 	RetryInterval        = time.Second * 10
-	Timeout              = time.Second * 1200
+	Timeout              = time.Second * 600
 	CleanupRetryInterval = time.Second * 5
 	CleanupTimeout       = time.Second * 20
 )
