58a7b3bc89b637ca88889d5b353ee630ae89d983
Cyril Scetbon <cscetbon@gmail.com>
2019-11-20 19:38:11 -0500

    pod-status - Make code testable and add unit test

---
+++ pkg/controller/cassandracluster/pod_operation_test.go
@@ -0,0 +1,90 @@
+// Copyright 2019 Orange
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// 	You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// 	See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cassandracluster
+
+import (
+	"testing"
+
+	api "github.com/Orange-OpenSource/cassandra-k8s-operator/pkg/apis/db/v1alpha1"
+	"github.com/stretchr/testify/assert"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func TestPodsSlice(t *testing.T) {
+	assert := assert.New(t)
+
+	rcc, cc := helperInitCluster(t, "cassandracluster-2DC.yaml")
+	status := cc.Status.DeepCopy()
+	rcc.updateCassandraStatus(cc, status)
+
+	operatorName := "new-name"
+	oldOperatorName := "old-name"
+	operationName := "cleanup"
+	dcRackName := "dc1-rack1"
+	podLastOperation := &status.CassandraRackStatus[dcRackName].PodLastOperation
+	podLastOperation.Name = operationName
+	podLastOperation.Status = api.StatusOngoing
+	podLastOperation.OperatorName = oldOperatorName
+
+	podsSlice, checkOnly := rcc.podsSlice(cc, status, *podLastOperation, dcRackName, operationName, operatorName)
+
+	assert.Equal(len(podsSlice), 0)
+	assert.Equal(checkOnly, true)
+
+	podLastOperation.Status = api.StatusDone
+
+	podsSlice, checkOnly = rcc.podsSlice(cc, status, *podLastOperation, dcRackName, operationName, operatorName)
+
+	assert.Equal(len(podsSlice), 0)
+	assert.Equal(checkOnly, false)
+
+	podLastOperation.Status = api.StatusOngoing
+	podLastOperation.OperatorName = operatorName
+	assert.Equal(len(podsSlice), 0)
+	assert.Equal(checkOnly, false)
+
+	//Create the Pods wanted by the statefulset dc2-rack1 (1 node)
+	pod := &v1.Pod{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Pod",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "cassandra-demo-dc1-rack1-0",
+			Namespace: "ns",
+			Labels: map[string]string{
+				"app":                                  "cassandracluster",
+				"cassandracluster":                     "cassandra-demo",
+				"cassandraclusters.db.orange.com.dc":   "dc1",
+				"cassandraclusters.db.orange.com.rack": "rack1",
+				"cluster":                              "k8s.pic",
+				"dc-rack":                              "dc1-rack1",
+			},
+		},
+	}
+	pod.Status.Phase = v1.PodRunning
+	rcc.CreatePod(pod)
+
+	podLastOperation.Pods = []string{"cassandra-demo-dc1-rack1-0"}
+	podLastOperation.OperatorName = oldOperatorName
+
+	podsSlice, checkOnly = rcc.podsSlice(cc, status, *podLastOperation, dcRackName, operationName, operatorName)
+
+	assert.Equal(podsSlice, []v1.Pod{*pod})
+	assert.Equal(len(podsSlice), 1)
+	assert.Equal(checkOnly, true)
+
+}
