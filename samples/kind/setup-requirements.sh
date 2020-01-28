0629515b77ddb7be1f905e5463d9344674ec4bea
Cyril Scetbon <cscetbon@gmail.com>
2020-01-28 18:18:02 -0500

    tolerations - Remove helm init kind for helm3 users

--- samples/kind/setup-requirements.sh
+++ samples/kind/setup-requirements.sh
@@ -4,5 +4,4 @@ NAMESPACE=${1:-cassandra-e2e}
 $(dirname $0)/configure-local-storage.sh create
 kubectl create namespace $NAMESPACE
 kubens $NAMESPACE
-helm init --service-account tiller
 kubectl apply -f deploy/crds/db_v1alpha1_cassandracluster_crd.yaml
