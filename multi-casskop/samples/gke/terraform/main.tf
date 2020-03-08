118f44edde6395ecc3e859a6a3b0d8a6bc060b32
erdrix <aguitton.ext@orange.com>
2020-03-08 19:40:33 +0100

    prepare release 0.5.2

--- multi-casskop/samples/gke/terraform/main.tf
+++ multi-casskop/samples/gke/terraform/main.tf
@@ -68,7 +68,7 @@ variable "managed_zone" {
 variable "casskop_image_tag" {
   description = ""
   type        = string
-  default     = "v0.5.1-release"
+  default     = "v0.5.2-release"
 }
 
 // Provider definition
