52ddab0070b126d039514b19ab56aaa359faf8b3
erdrix <aguitton.ext@orange.com>
2020-05-05 11:40:37 +0200

    wip backrest sidecar

--- pkg/apis/db/v1alpha1/cassandracluster_types.go
+++ pkg/apis/db/v1alpha1/cassandracluster_types.go
@@ -26,6 +26,10 @@ import (
 )
 
 const (
+	// Backup Restore default config
+	DefaultBackRestSidecarImage				string = "gcr.io/cassandra-operator/cassandra-sidecar:v6.2.0"
+	DefaultBackRestSidecarContainerPort		int32 = 4567
+
 
 	DefaultLivenessInitialDelaySeconds 	int32 = 120
 	DefaultLivenessHealthCheckTimeout  	int32 = 20
@@ -162,6 +166,17 @@ func (cc *CassandraCluster) CheckDefaults() {
 	if ccs.ReadinessHealthCheckPeriod == nil {
 		ccs.ReadinessHealthCheckPeriod = func(i int32) *int32 { return &i }(DefaultReadinessHealthCheckPeriod)
 	}
+
+	// BackupRestore default config
+	if ccs.BackRestSidecar == nil {
+		ccs.BackRestSidecar = &BackRestSidecar{}
+	}
+	if len(ccs.BackRestSidecar.Image) == 0 {
+		ccs.BackRestSidecar.Image = DefaultBackRestSidecarImage
+	}
+	if ccs.BackRestSidecar.ContainerPort == nil {
+		ccs.BackRestSidecar.ContainerPort = func(i int32) *int32 { return &i }(DefaultBackRestSidecarContainerPort)
+	}
 }
 
 // SetDefaults sets the default values for the cassandra spec and returns true if the spec was changed
@@ -185,6 +200,7 @@ func (cc *CassandraCluster) SetDefaults() bool {
 	}
 	if ccs.MaxPodUnavailable == 0 {
 		ccs.MaxPodUnavailable = defaultMaxPodUnavailable
+		changed = true
 	}
 	if cc.Spec.Resources.Limits == (CPUAndMem{}) {
 		cc.Spec.Resources.Limits = cc.Spec.Resources.Requests
@@ -852,6 +868,8 @@ type CassandraClusterSpec struct {
 	// ReadinessSuccessThreshold defines success threshold for the readiness probe of the main
 	// cassandra container : https://kubernetes.io/docs/tasks/configure-pod-container/configure-liveness-readiness-startup-probes/#configure-probes
 	ReadinessSuccessThreshold *int32 `json:"readinessSuccessThreshold,omitempty"`
+
+	BackRestSidecar *BackRestSidecar `json:"backRestSidecar,omitempty"`
 }
 
 // StorageConfig defines additional storage configurations
@@ -941,6 +959,14 @@ type CPUAndMem struct {
 	CPU string `json:"cpu"`
 	// +kubebuilder:validation:Pattern=^([+-]?[0-9.]+)([eEinumkKMGTP]*[-+]?[0-9]*)$
 	Memory string `json:"memory"`
+}
+
+type BackRestSidecar struct {
+	Image 				string						`json:"image,omitempty"`
+	ImagePullPolicy		v1.PullPolicy 				`json:"imagePullPolicy,omitempty"`
+//	SecretVolumeSource	*v1.SecretVolumeSource		`json:"sidecarSecretVolumeSource,omitempty"`
+	Resources			*v1.ResourceRequirements	`json:"resources,omitempty"`
+	ContainerPort		*int32						`json:"containerPort,omitempty"`
 }
 
 //CassandraRackStatus defines states of Cassandra for 1 rack (1 statefulset)
