521d8b4ddfd9b2868a33f6ea08108741c50992b2
erdrix <aguitton.ext@orange.com>
2020-06-18 23:49:19 +0200

    review changes

--- pkg/sidecarclient/common.go
+++ pkg/sidecarclient/common.go
@@ -8,4 +8,4 @@ var ErrCassandraSidecarNotConnected 	    = errors.New("The targeted sidecar is d
 var ErrCassandraSidecarNotReturned200 	    = errors.New("non 200 response from sidecar cluster")
 var ErrCassandraSidecarNotReturned201 	    = errors.New("non 201 response from sidecar cluster")
 var ErrCassandraSidecarReturned404 		    = errors.New("404 response from sidecar cluster")
-var ErrNoCassandraSidecarClientsAvailable	= errors.New("Cannot create a sidecar client to perform actions")
+var ErrNoCassandraSidecarClientsAvailable   = errors.New("Cannot create a sidecar client to perform actions")
