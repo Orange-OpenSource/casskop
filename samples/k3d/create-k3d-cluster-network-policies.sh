8e1deee8b3cbdff65fa0114ed34132ecc88d8e32
Cyril Scetbon <cscetbon@gmail.com>
2021-02-21 18:39:21 -0500

    Update network policies

---
+++ samples/k3d/create-k3d-cluster-network-policies.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+curl -o calico.yaml https://raw.githubusercontent.com/rancher/k3d/main/docs/usage/guides/calico.yaml
+CLUSTER=local-casskop
+k3d cluster delete $CLUSTER
+k3d cluster create $CLUSTER --k3s-server-arg '--flannel-backend=none' \
+  --volume "calico.yaml:/var/lib/rancher/k3s/server/manifests/calico.yaml"
+. $(dirname $0)/setup-requirements.sh
+kubectl apply -f $(dirname $0)/../network-policies.yaml
